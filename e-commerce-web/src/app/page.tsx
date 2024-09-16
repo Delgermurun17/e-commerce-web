831bd91c8a7e4abfb2ed320fc5aa54bf61c578ff
JavzaaBatmunkh <javzaabatmunkh@gmail.com>
2024-09-16 15:17:36 +0800

    page

--- e-commerce-web/src/app/page.tsx
+++ e-commerce-web/src/app/page.tsx
@@ -1,11 +1,14 @@
-import Categories from "@/components/categories";
 import ProductCard from "@/components/productCard";
-
 export default function Home() {
   return (
     <main>
-      {/* <ProductCard/>  */}
-      <Categories/>
+      <ProductCard />
+      <div className="grid grid-cols-4 gap-4">
+        {[...Array(15)].map((_, index: number) => (
+          <ProductCard key={index} className={index === 7 || index === 8 ? ` col-span-2 row-span-2` : ``} />
+        ))}
+      </div>
+
     </main>
   );
 }
