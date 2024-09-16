3ff2d2b2fabd3850ee3413058aec37141e3f0965
Ts3nguun <0805tsenguun@gmail.com>
2024-09-16 19:37:04 +0800

    last step of payment

---
+++ e-commerce-web/src/components/PaymentSection.tsx
@@ -0,0 +1,8 @@
+export default function PaymentSection() {
+    return(
+        <div>
+            <div className="w-[687px] h-[656px] bg-white mx-auto pt">
+            </div>
+        </div>
+    )
+}
