7c75eddf15a3f3be8385e8ecb894238eda63e4cf
Delgermurun17 <88delgeree@gmail.com>
2024-09-17 17:10:52 +0800

    solved all conflict done

--- e-commerce-web/src/app/category/page.tsx
+++ e-commerce-web/src/app/category/page.tsx
@@ -1,19 +1,20 @@
 import Categories from "@/components/categories";
 import ProductCard from "@/components/productCard";
+import { Suspense } from "react";
 
 export default function Page() {
-    return (
-        <div className="md:px-[14%] px-[5%] grid grid-cols-4 py-[5%] max-w-[1600px] mx-auto gap-2">
-            <div>
-                <Categories />
-            </div>
-            <div className="col-span-3 grid md:grid-cols-3 grid-cols-2 gap-5">
-              
-                    {
-                        [...Array(15)].map((_, index) => (
-                            <ProductCard key={index} />))
-                    }
-            </div>
-        </div>
-    )
+  return (
+    <div className="md:px-[14%] px-[5%] grid grid-cols-4 py-[5%] max-w-[1600px] mx-auto gap-2">
+      <div>
+        <Suspense>
+          <Categories />
+        </Suspense>
+      </div>
+      <div className="col-span-3 grid md:grid-cols-3 grid-cols-2 gap-5">
+        {[...Array(15)].map((_, index) => (
+          <ProductCard key={index} />
+        ))}
+      </div>
+    </div>
+  );
 }
