c1e1ec8e0a2673c0733040f993e1571669aa7414
Jerry0129u <innerdot559@gmail.com>
2024-09-24 19:38:03 +0800

    home fixed

--- e-commerce-web/src/app/category/page.tsx
+++ e-commerce-web/src/app/category/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     <div className="md:px-[14%] px-[5%] grid grid-cols-4 max-w-[1600px] mx-auto gap-2 pt-14 pb-24">
       <div>
         <Suspense>
-          <Categories />
+          <Categories />  
         </Suspense>
       </div>
       <div className="col-span-3 grid md:grid-cols-3 grid-cols-2 gap-5">
