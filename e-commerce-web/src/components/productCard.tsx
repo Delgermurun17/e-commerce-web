a95196eea5b43ef74ac5ab8d2c5d7fd8591f111e
Jerry0129u <innerdot559@gmail.com>
2024-09-25 18:52:59 +0800

    Fix on main

--- e-commerce-web/src/components/productCard.tsx
+++ e-commerce-web/src/components/productCard.tsx
@@ -13,16 +13,16 @@ export default function ProductCard(props: Props) {
 
     return (
         <div className={className}>
-            <div className="relative aspect-[3/4] bg-slate-400 rounded-xl overflow-hidden " >
-                <Image className="hover:scale-125 duration-700" src="/image.jpg" width={2000} height={2000} alt="picture" />
+            <div className="relative aspect-[3/4] bg-slate-400 rounded-xl overflow-hidden">
+                <Image className="hover:scale-125 duration-700" src="/card.png" width={2000} height={2000} alt="picture" />
                 <button className="absolute top-4 right-4" onClick={() => setIsSaved(x => !x)}>
-                <Heart onClick={()=>setIsSaved(x =>!x)} strokeWidth={1} fill={`${isSaved ? "black" : "transparent"}`} className="duration-500"/>
+                    <Heart strokeWidth={1} fill={isSaved ? "black" : "transparent"} className="duration-500" />
                 </button>
             </div>
-            <div className="md:text-base text-sm">
-                <p className="font-normal">Product Name</p>
-                <p className="font-bold">Price₮</p>
+            <div className="text-base leading-6">
+                <p className="font-normal">Local Styles Crewneck</p>
+                <p className="font-bold">120.000₮</p>
             </div>
         </div>
-    )
+    );
 }
