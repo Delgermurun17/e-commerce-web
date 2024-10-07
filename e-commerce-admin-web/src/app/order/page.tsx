9b0bfa72099820b2a5ad71e8fd0d6f0396417c6d
Ts3nguun <0805tsenguun@gmail.com>
2024-10-07 18:02:48 +0800

    done

--- e-commerce-admin-web/src/app/order/page.tsx
+++ e-commerce-admin-web/src/app/order/page.tsx
@@ -1,7 +1,10 @@
 import Menu from "@/components/menu";
 
-export default function Page(){
-    return(<div className="flex bg-[#FFFFFF] text-black">
-        <Menu/>
-    </div>)
+export default function Page() {
+    return (
+        <div className="flex bg-[#FFFFFF] text-black">
+            <Menu />
+            
+        </div>
+    )
 }
